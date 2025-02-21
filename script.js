af6b9584bc8fbf1e17df73a41c35d3a438b35ab8
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2025-02-21 12:25:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ window.onload = () => { //window
     }
 }
 
-let playerXIcon = "fa fa-times"; //class name from fontawesome icon
-let playerOIcon = "fa fa-circle"; //class name from fontawesome icon
+let playerXIcon = "fa-solid fa-xmark"; //class name from fontawesome icon
+let playerOIcon = "fa-regular fa-circle"; //class name from fontawesome icon
 let playerSign = "X";
 let runBot = true;
 
